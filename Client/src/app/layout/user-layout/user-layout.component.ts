2b80e3e3dba9f0cda95ee4f10b76735863f3e7c2
ha752002 <honggha75@gmail.com>
2023-04-26 20:23:56 +0700

    cart

--- Client/src/app/layout/user-layout/user-layout.component.ts
+++ Client/src/app/layout/user-layout/user-layout.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-layout',
   templateUrl: './user-layout.component.html',
-  styleUrls: ['./user-layout.component.css']
+  styleUrls: ['./user-layout.component.css'],
+  
 })
 export class UserLayoutComponent {
 
